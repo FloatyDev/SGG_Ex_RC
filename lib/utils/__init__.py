2b1d4a0eb63e03a00f85b93f67573b7523e2c870
Giorgos Sygkelakis <sygkelakis@ics.forth.gr>
2024-07-26 10:28:38 +0300

    fix(package): fix paths os sys path

--- lib/utils/__init__.py
+++ lib/utils/__init__.py
@@ -7,7 +7,7 @@
 from . import cython_nms
 from . import cython_bbox
 import sys
-sys.path.append("/u/zkou2/Code/FactorizableNet/lib/utils")
+sys.path.append("./lib/utils")
 import blob
 import nms
 import timer
